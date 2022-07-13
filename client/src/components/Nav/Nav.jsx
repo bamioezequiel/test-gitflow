78572e1c5612bf3a39bd0e13ef8c82a19cc601fb
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-07-13 13:26:43 -0300

    new nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Nav() {
     return (
         <div>
-            
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About me</a>
+                <a href="#">Login</a>
+            </nav>
         </div>
     )
 }
