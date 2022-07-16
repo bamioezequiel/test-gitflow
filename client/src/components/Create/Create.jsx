d744aa18c4a8ee7db46aedc024a64dafb816531f
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-07-15 23:20:08 -0300

    create

---
+++ client/src/components/Create/Create.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import validations from './validations.js';
+
+export default function Create() {
+
+    
+
+    return (
+        <div>
+            <h2>Create</h2>
+        </div>
+    )
+}
