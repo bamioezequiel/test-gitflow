d2021cc8e6282413423664a5d8b7c02de3c9c5eb
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-07-15 23:18:05 -0300

    testing rules for admin

---
+++ client/src/components/Create/Create.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import validations from './validations.js';
+
+export default function Create() {
+
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
